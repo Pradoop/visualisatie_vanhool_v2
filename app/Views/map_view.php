b443555d8bd27b6ad831d05d36f650e2ba3a2d8c
yannick.guilliams <yannick.guilliams@student.kuleuven.be>
2022-04-10 10:56:37 +0200

    css map_view

--- app/Views/map_view.php
+++ app/Views/map_view.php
@@ -1,5 +1,3 @@
-//TODO: Show enkel chassis nummers + popup met meer informatie
-
 <div id="map_div">
     <img id="map" class="site-map" src="<?= base_url()?>/images/map_hal5_hal6.png" alt="...">
 </div>
@@ -8,37 +6,39 @@
     <h1>Important Chassis</h1>
     <div class="grid-container">
         <?php $div_counter = 1; ?>
-        <?php foreach($chassis_info as $c): ?>
+        <?php $i = 1; ?>
+        <?php while($i < sizeof($chassis_info)): ?>
 
             <?php if($div_counter == 1): ?>
                 <div class="grid-child left">
-                    <p id="<?= 'chassis_'.$c[1]?>"><?= $c[1] ?></p>
+                    <p id="<?= 'chassis_'.$chassis_info[$i][1]?>"><?= $chassis_info[$i][1] ?></p>
                 </div>
             <?php endif; ?>
 
             <?php if($div_counter == 2): ?>
                 <div class="grid-child center_left">
-                    <p id="<?= 'chassis_'.$c[1]?>"><?= $c[1] ?></p>
+                    <p id="<?= 'chassis_'.$chassis_info[$i][1]?>"><?= $chassis_info[$i][1] ?></p>
                 </div>
             <?php endif; ?>
 
             <?php if($div_counter == 3): ?>
                 <div class="grid-child center_right">
-                    <p id="<?= 'chassis_'.$c[1]?>"><?= $c[1] ?></p>
+                    <p id="<?= 'chassis_'.$chassis_info[$i][1]?>"><?= $chassis_info[$i][1] ?></p>
                 </div>
             <?php endif; ?>
 
             <?php if($div_counter == 4): ?>
                 <div class="grid-child right">
-                    <p id="<?= 'chassis_'.$c[1]?>"><?= $c[1] ?></p>
+                    <p id="<?= 'chassis_'.$chassis_info[$i][1]?>"><?= $chassis_info[$i][1] ?></p>
                 </div>
             <?php endif; ?>
 
+            <?php $i++; ?>
             <?php $div_counter++; ?>
             <?php if($div_counter > 4): ?>
                 <?php $div_counter = 1; ?>
             <?php endif; ?>
 
-        <?php endforeach; ?>
+        <?php endwhile; ?>
     </div>
 </div>
