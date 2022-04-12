ec0bbf9589053a99f8e61ffad6ced4d213414bb4
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2022-04-12 10:00:49 +0200

    commit for local path for file

--- app/Models/FileHandler.php
+++ app/Models/FileHandler.php
@@ -15,7 +15,8 @@ class FileHandler extends \CodeIgniter\Model
         $main_array = array();
         //$file = file("C:\Users\YAGU\Documents\ChassisInKaliberIVCopy.txt");
         //$file = file("\\ivserver\mainframe\Student\ChassisInKaliberIV.txt");
-        $file = file("C:\Users\Yanni\OneDrive\Documenten\Master's Thesis (20sp)\TestFile.txt");
+        //$file = file("C:\Users\Yanni\OneDrive\Documenten\Master's Thesis (20sp)\TestFile.txt");
+        $file = file("C:\Users\pradk\Documents\Uni\Thesis\TestFile.txt");
         $count_lines = count($file);
         $i = 0;
         while($i < $count_lines) {
