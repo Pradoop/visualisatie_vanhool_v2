68725e31de2dafb9bc8c12750eb4bcad0708506f
yannick.guilliams <yannick.guilliams@student.kuleuven.be>
2022-04-22 11:40:56 +0200

    correct reading textfile on map view

--- app/Views/map_view.php
+++ app/Views/map_view.php
@@ -11,35 +11,35 @@
         <div class="grid-child col1">
             <?php $i = 1; ?>
             <?php while($i < sizeof($chassis_info)): ?>
-                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][1] ?></p>
+                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][0] ?></p>
                 <?php $i = $i + 5; ?>
             <?php endwhile; ?>
         </div>
         <div class="grid-child col2">
             <?php $i = 2; ?>
             <?php while($i < sizeof($chassis_info)): ?>
-                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][1] ?></p>
+                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][0] ?></p>
                 <?php $i = $i + 5; ?>
             <?php endwhile; ?>
         </div>
         <div class="grid-child col3">
             <?php $i = 3; ?>
             <?php while($i < sizeof($chassis_info)): ?>
-                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][1] ?></p>
+                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][0] ?></p>
                 <?php $i = $i + 5; ?>
             <?php endwhile; ?>
         </div>
         <div class="grid-child col4">
             <?php $i = 4; ?>
             <?php while($i < sizeof($chassis_info)): ?>
-                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][1] ?></p>
+                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][0] ?></p>
                 <?php $i = $i + 5; ?>
             <?php endwhile; ?>
         </div>
         <div class="grid-child col5">
             <?php $i = 5; ?>
             <?php while($i < sizeof($chassis_info)): ?>
-                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][1] ?></p>
+                <p id="<?= 'chassis_'.$i?>"><?= $chassis_info[$i][0] ?></p>
                 <?php $i = $i + 5; ?>
             <?php endwhile; ?>
         </div>
--- public/js/map_view.js
+++ public/js/map_view.js
@@ -14,15 +14,24 @@ var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
 for(let i = 1; i < chassis_info.length; i++) {
     $('#chassis_' + i).popover({
         trigger: 'click',
-        title: 'Chassis: ' + chassis_info[i][1],
-        content: 'DLnr: ' + chassis_info[i][2] + '\n' +
-            'Kaliber: ' + chassis_info[i][3] + ' ' + chassis_info[i][4] + '\n' +
-            'NaamFase: ' + chassis_info[i][5] + '\n' +
-            'NaamKlant: ' + chassis_info[i][6] + ' ' + chassis_info[i][7] + ' ' + chassis_info[i][8]  + '\n' +
-            'NaamType: ' + chassis_info[i][9] + '\n' +
-            'Natie: ' + chassis_info[i][10] + '\n' +
-            'StandInProd: ' + chassis_info[i][11] + '\n' +
-            'ReeksVan: ' + chassis_info[i][12] + '\n' +
-            'ReeksTot: ' + chassis_info[i][13] + '\n'
+        title: chassis_info[0][0] + ': ' + chassis_info[i][0],
+        content: chassis_info[0][1] + ': ' + chassis_info[i][1] + '\n' +
+            chassis_info[0][2] + ': ' + chassis_info[i][2] + '\n' +
+            chassis_info[0][3] + ': ' + chassis_info[i][3] + '\n' +
+            chassis_info[0][4] + ': ' + chassis_info[i][4] + '\n' +
+            chassis_info[0][5] + ': ' + chassis_info[i][5] + '\n' +
+            chassis_info[0][6] + ': ' + chassis_info[i][6] + '\n' +
+            chassis_info[0][7] + ': ' + chassis_info[i][7] + '\n' +
+            chassis_info[0][8] + ': ' + chassis_info[i][8] + '\n' +
+            chassis_info[0][9] + ': ' + chassis_info[i][9] + '\n' +
+            chassis_info[0][10] + ': ' + chassis_info[i][10] + '\n' +
+            chassis_info[0][11] + ': ' + chassis_info[i][11] + '\n' +
+            chassis_info[0][12] + ': ' + chassis_info[i][12] + '\n' +
+            chassis_info[0][13] + ': ' + chassis_info[i][13] + '\n' +
+            chassis_info[0][14] + ': ' + chassis_info[i][14] + '\n' +
+            chassis_info[0][15] + ': ' + chassis_info[i][15] + '\n' +
+            chassis_info[0][16] + ': ' + chassis_info[i][16] + '\n' +
+            chassis_info[0][17] + ': ' + chassis_info[i][17] + '\n' +
+            chassis_info[0][18] + ': ' + chassis_info[i][18] + '\n'
     });
 }
