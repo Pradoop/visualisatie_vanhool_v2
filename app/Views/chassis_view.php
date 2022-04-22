f2ac4f9ff1ecb1c58028fa86717a41d588590f41
yannick.guilliams <yannick.guilliams@student.kuleuven.be>
2022-04-22 11:31:23 +0200

    correct reading textfile on chassis view

--- app/Views/chassis_view.php
+++ app/Views/chassis_view.php
@@ -1,7 +1,7 @@
 <script > let chassis_info = <?php echo json_encode($chassis_info); ?>;</script>
 
 <div class="grid-container">
-    <?php $kolom_nr = 1; ?>
+    <?php $kolom_nr = 0; ?>
     <div class="grid-child">
         <!-- Wagen !-->
         <h2><?= $chassis_info[0][0] ?></h2>
@@ -17,7 +17,7 @@
         <h2><?= $chassis_info[0][1] ?></h2>
         <?php $lijn_nr = 1; ?>
         <?php while($lijn_nr < sizeof($chassis_info)): ?>
-            <?php if($chassis_info[$lijn_nr][$kolom_nr] == '*'): ?>
+            <?php if($chassis_info[$lijn_nr][$kolom_nr] != ' '): ?>
                 <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
             <?php else: ?>
                 <p>&nbsp;</p>
@@ -31,11 +31,7 @@
         <h2><?= $chassis_info[0][2] ?></h2>
         <?php $lijn_nr = 1; ?>
         <?php while($lijn_nr < sizeof($chassis_info)): ?>
-            <?php if(strlen((string)$chassis_info[$lijn_nr][$kolom_nr]) != 6): ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
-            <?php else: ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr - 1] ?></p>
-            <?php endif; ?>
+            <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
             <?php $lijn_nr++; ?>
         <?php endwhile; ?>
     </div>
@@ -45,11 +41,7 @@
         <h2><?= $chassis_info[0][3] ?></h2>
         <?php $lijn_nr = 1; ?>
         <?php while($lijn_nr < sizeof($chassis_info)): ?>
-            <?php if(strlen((string)$chassis_info[$lijn_nr][$kolom_nr]) == 6): ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
-            <?php else: ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr - 1] ?></p>
-            <?php endif; ?>
+            <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
             <?php $lijn_nr++; ?>
         <?php endwhile; ?>
     </div>
@@ -59,11 +51,7 @@
         <h2><?= $chassis_info[0][4] ?></h2>
         <?php $lijn_nr = 1; ?>
         <?php while($lijn_nr < sizeof($chassis_info)): ?>
-            <?php if(is_numeric($chassis_info[$lijn_nr][$kolom_nr])): ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
-            <?php else: ?>
-                <p><?= $chassis_info[$lijn_nr][$kolom_nr - 1] ?></p>
-            <?php endif; ?>
+            <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
             <?php $lijn_nr++; ?>
         <?php endwhile; ?>
     </div>
@@ -143,7 +131,11 @@
         <h2><?= $chassis_info[0][12] ?></h2>
         <?php $lijn_nr = 1; ?>
         <?php while($lijn_nr < sizeof($chassis_info)): ?>
-            <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
+            <?php if($chassis_info[$lijn_nr][$kolom_nr] != ' '): ?>
+                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
+            <?php else: ?>
+                <p>&nbsp;</p>
+            <?php endif; ?>
             <?php $lijn_nr++; ?>
         <?php endwhile; ?>
     </div>
@@ -181,18 +173,38 @@
     <div class="grid-child">
         <!-- wdTeLaat !-->
         <h2><?= $chassis_info[0][16] ?></h2>
-
+        <?php $lijn_nr = 1; ?>
+        <?php while($lijn_nr < sizeof($chassis_info)): ?>
+            <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
+            <?php $lijn_nr++; ?>
+        <?php endwhile; ?>
     </div>
     <?php $kolom_nr++; ?>
     <div class="grid-child">
         <!-- wdInMont !-->
         <h2><?= $chassis_info[0][17] ?></h2>
-
+        <?php $lijn_nr = 1; ?>
+        <?php while($lijn_nr < sizeof($chassis_info)): ?>
+            <?php if(is_numeric($chassis_info[$lijn_nr][$kolom_nr])): ?>
+                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
+            <?php else: ?>
+                <p>&nbsp;</p>
+            <?php endif; ?>
+            <?php $lijn_nr++; ?>
+        <?php endwhile; ?>
     </div>
     <?php $kolom_nr++; ?>
     <div class="grid-child">
         <!-- standLas !-->
         <h2><?= $chassis_info[0][18] ?></h2>
-
+        <?php $lijn_nr = 1; ?>
+        <?php while($lijn_nr < sizeof($chassis_info)): ?>
+            <?php if(str_starts_with($chassis_info[$lijn_nr][$kolom_nr], 'L')): ?>
+                <p><?= $chassis_info[$lijn_nr][$kolom_nr] ?></p>
+            <?php else: ?>
+                <p>&nbsp;</p>
+            <?php endif; ?>
+            <?php $lijn_nr++; ?>
+        <?php endwhile; ?>
     </div>
 </div>
