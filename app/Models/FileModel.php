c72ee9c98a6daf65918f2445b46e713523e5cc0a
pradeep.sureshkumar <pradeep.sureshkumar@student.kuleuven.be>
2022-04-25 10:53:20 +0200

    general information for dashboard

--- app/Models/FileModel.php
+++ app/Models/FileModel.php
@@ -13,7 +13,7 @@ class FileModel extends \CodeIgniter\Model
     public function readFile()
     {
         $main_array = array();
-        $file = file("");
+        $file = file("C:\Users\pradk\Documents\Uni\Thesis\VanHoolTestFile.txt");
         $count_lines = count($file);
         $i = 0;
         while($i < $count_lines) {
--- app/Views/analyze_view.php
+++ app/Views/analyze_view.php
@@ -1,2 +1,36 @@
+<script src="https://d3js.org/d3.v7.min.js"></script>
+
+
+<div id="general_information">
+    <h1>Dashboard</h1>
+    <div class="grid-container">
+        <div class="grid-child col1">
+            <div class="information_1">
+                <p class="information_text"> Aantal chassis in productie:</p>
+            </div>
+        </div>
+        <div class="grid-child col2">
+            <div class="information_2">
+                <p class="information_text"> Percentage chassis vertraagd:</p>
+            </div>
+        </div>
+        <div class="grid-child col3">
+            <div class="information_3">
+                <p class="information_text"> Gemiddeld aantal werkdagen te laat:</p>
+            </div>
+        </div>
+        <div class="grid-child col4">
+            <div class="information_4">
+                <p class="information_text"> Percentage chassis in productielijn:</p>
+            </div>
+        </div>
+        <div class="grid-child col5">
+            <div class="information_5">
+                <p class="information_text"> TBA</p>
+            </div>
+        </div>
+    </div>
+
+</div>
 
 
