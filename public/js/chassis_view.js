39ec844a4275c2c7d72a50cccb03cba4b32efe01
YannickGuilliams <yannick.guilliams@student.kuleuven.be>
2022-05-21 13:08:28 +0200

    chassis_view: table fix

--- public/js/chassis_view.js
+++ public/js/chassis_view.js
@@ -1,13 +1,3 @@
-//Search Box
-$(document).ready(function(){
-    $("#search_input").on("keyup", function() {
-        let value = $(this).val().toLowerCase();
-        $("#myTable tr").filter(function() {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-        });
-    });
-});
-
 //Table
 $(document).ready( function () {
     $('#chassis_table').DataTable();
